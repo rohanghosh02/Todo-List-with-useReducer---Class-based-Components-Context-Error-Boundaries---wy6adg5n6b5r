2b983c1cf98548b76a64dbdcb358e48e2bf01244
Rohan <rohankingkumar8@gmail.com>
2022-10-29 20:15:37 +0530

    add

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,13 +2,10 @@ import React, { useState } from "react";
 const Todo = ({ todo, remove }) => {
   return (
     <div className="todo">
-      <div className="todo-title">
-        {todo.text}
-
-        <button className="todo-delete" onClick={remove}>
-          X
-        </button>
-      </div>
+      <div className="todo-title">{todo.text}</div>
+      <button className="todo-delete" onClick={remove}>
+        X
+      </button>
 
       {/* <button className="EditTodo" onClick={() => setMode("edit")}>
             Edit
